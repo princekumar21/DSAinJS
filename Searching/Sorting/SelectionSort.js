4b21051a8fea4775589103dcb28a2a864a839c16
Prince Kumar <69597958+princekumar21@users.noreply.github.com>
2026-01-03 11:45:31 +0000

    selection sort time complexity

--- Searching/Sorting/SelectionSort.js
+++ Searching/Sorting/SelectionSort.js
@@ -9,14 +9,14 @@ var selectionSort = function (nums) {
     if (nums.length === 0) return -1;
     if (nums.length === 1) return nums;
 
-    for(let i = 0; i < nums.length - 1; i++){
+    for (let i = 0; i < nums.length - 1; i++) {
         let min = i;
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[j] < nums[min]){
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[min]) {
                 min = j;
             }
         }
-        if(min !== i){
+        if (min !== i) {
             swapItem(nums, i, min);
         }
     }
@@ -25,3 +25,4 @@ var selectionSort = function (nums) {
 
 console.log(selectionSort([4, 9, 2, -4, 1]));
 
+// Tiem Complexity O(n^2)
